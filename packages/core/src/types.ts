ef90feca2a626d6ca2dae2910079966a22af836d
MatsThieme <mats.thieme.mt@gmail.com>
2023-06-22 00:25:28 +0200

    feat: add typecheck and lint to release workflow

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { InputAxis } from "./InputAxis/InputAxis";
 import type { InputButton } from "./InputButton/InputButton";
 import type { InputAdapter } from "./InputAdapter";
